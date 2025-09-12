bcec793db4c9732978c6fff9d505806b5fcdafd0
AnapatD <rafa10072549@gmail.com>
2025-09-12 15:52:06 +0700

    2.3-3.0

---
+++ cell03/ex00/script.js
@@ -0,0 +1,3 @@
+function changeBackground () {
+	document.body.style.backgroundColor = document.body.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+}
